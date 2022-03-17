e7d27aaf9016065bb487b0623e01babe2dc52421
Pasquale Richardson <Pasquale.Richardson@gmail.com>
2022-03-17 14:58:45 +0500

    Hello world draft

---
+++ src/main/java/org/prichardson/hello/Runner.java
@@ -0,0 +1,12 @@
+package org.prichardson.hello;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Runner {
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+    }
+
+}
